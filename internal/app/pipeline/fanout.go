534421995ea914a78cddd499f75491b02ec176d2
Tair <trouzerop@gmail.com>
2025-07-30 15:18:36 +0500

    implementing everthing

---
+++ internal/app/pipeline/fanout.go
@@ -0,0 +1,25 @@
+package pipeline
+
+import (
+	"marketflow/internal/models/domain"
+)
+
+func FanOut(in <-chan domain.PriceUpdate, workerCount int) []chan domain.PriceUpdate {
+	workerChans := make([]chan domain.PriceUpdate, workerCount)
+	for i := range workerChans {
+		workerChans[i] = make(chan domain.PriceUpdate, 100)
+	}
+
+	go func() {
+		i := 0
+		for update := range in {
+			workerChans[i%workerCount] <- update
+			i++
+		}
+		for _, ch := range workerChans {
+			close(ch)
+		}
+	}()
+
+	return workerChans
+}
---
+++ internal/app/pipeline/workerpool.go
@@ -0,0 +1,29 @@
+package pipeline
+
+import (
+	"context"
+	"marketflow/internal/app/logger"
+	"marketflow/internal/models/domain"
+)
+
+type Worker struct {
+	ID     int
+	Input  <-chan domain.PriceUpdate
+	Cache  domain.Cache
+	Output chan<- domain.PriceUpdate
+}
+
+func (w *Worker) Start(ctx context.Context) {
+	go func() {
+		for update := range w.Input {
+			err := w.Cache.SetLatest(ctx, update)
+			if err != nil {
+				logger.Error("cache error", "worker", w.ID, "error", err)
+			}
+
+			if w.Output != nil {
+				w.Output <- update
+			}
+		}
+	}()
+}
